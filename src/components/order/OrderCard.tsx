68c00fced58d5e5e58043980fab390c87b3b4e0e
Dzmitry <dimaau1@gmail.com>
2025-05-20 15:13:03 +0300

    fix index key

--- src/components/order/OrderCard.tsx
+++ src/components/order/OrderCard.tsx
@@ -12,9 +12,9 @@ const OrderCard = ({ order }: Props) => (
       <span className='text-base font-bold text-green-700 dark:text-green-400'>Total: ${order.total}</span>
     </div>
     <div className='flex flex-wrap gap-4'>
-      {order.items.map((item, itemIdx) => (
+      {order.items?.map((item) => (
         <div
-          key={itemIdx}
+          key={item.id}
           className='flex flex-col bg-gray-100 dark:bg-gray-700 rounded-lg p-4 min-w-[180px] max-w-xs flex-1 border border-gray-200 dark:border-gray-600'
         >
           <p className='font-medium text-gray-900 dark:text-gray-100 mb-1'>{item.name}</p>
